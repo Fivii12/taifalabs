e6b322214f32cddf8810516734f4ccb9dda54ac6
Fivii12 <dryg882@gmail.com>
2024-12-22 19:53:30 +0300

    34fullready

--- GrammarIntoNka.py
+++ GrammarIntoNka.py
@@ -1,12 +1,17 @@
+from idlelib.configdialog import tracers
 from idlelib.pyparse import trans
 import pandas as pd
+
+from graphviz import Digraph
+
+is_right = False
 
 def parse_grammar(file_path):
     grammar = {}
     inner_transition = {}
     file = open(file_path)
-    is_right = False
     grammar['H'] = {}
+    global is_right
     for line in file:
         line = line.strip()
         print(line)
@@ -29,7 +34,9 @@ def parse_grammar(file_path):
                 else:
                     symbol = r.strip()[0]
                     transition = r.strip()[2:3]
+
             transition = transition.strip('<>')
+
             if not is_right: # левосторонняя
                 if left not in grammar: # если нет ключа делаем
                     grammar[left] = {}
@@ -46,10 +53,9 @@ def parse_grammar(file_path):
 
 
 def grammar_to_table(grammar):
-    # Создадим список всех символов для строк и столбцов
     terminals = sorted(set(symbol for transitions in grammar.values() for symbol in transitions.keys())) #терминалы
     nonterminals = sorted(grammar.keys()) # нетерминалы
-    # Подготовим таблицу
+
     table = []
     for nonterminal in nonterminals:
         row_data = []
@@ -61,15 +67,70 @@ def grammar_to_table(grammar):
                 row_data.append('-')
         table.append(row_data)
 
-    # Создаем DataFrame для красивого вывода
     df = pd.DataFrame(table, index=nonterminals, columns=terminals)
     return df.T
+
 def save_table(df, path):
     df.to_csv(path, sep=";")
-path = 'grammar_input_right.csv'
+path = 'grammar_test_lesson.csv'
 grammar = parse_grammar(path)
 print(grammar)
 df = grammar_to_table(grammar)
 print(df)
 save_path = 'NKA.csv'
 save_table(df, save_path)
+
+def nka_to_dka(grammar):
+    stack = []
+    dka = {}
+    processed = set()
+    if is_right:
+        stack.append('H')
+    else:
+        stack.append('S')
+    f = 0
+    while stack:
+        f += 1
+        print(f, f'current state: {stack}')
+
+        current_state = stack.pop()
+        transitions_by_symbol = {}
+        for state in current_state:
+            if state not in grammar:
+                continue
+
+            # символ и переход для состояния
+            for symbol, next_states in grammar[state].items():
+                print(symbol, next_states)
+                if symbol not in transitions_by_symbol:
+                    transitions_by_symbol[symbol] = []
+                # переходы для символа
+                transitions_by_symbol[symbol].extend(next_states)
+
+        print(f"transitions_by_symbol: {transitions_by_symbol}")
+
+        for symbol, transitions in transitions_by_symbol.items():
+            #соединяем для передачи
+            merged_state = ''.join(sorted(set(''.join(transition) for transition in transitions)))
+            if current_state not in dka:
+                dka[current_state] = {}
+            dka[current_state][symbol] = merged_state
+
+            if merged_state not in processed:
+                stack.append(merged_state)
+        processed.add(current_state)
+
+    return dka
+
+def make_final_graph(dka):
+    dot = Digraph(comment='Final State Machine')
+    for symbol, transitions in dka.items():
+        dot.node(symbol)
+    for symbol, transitions in dka.items():
+        for transition, state in transitions.items():
+            dot.edge(symbol, state, label=transition)
+    dot.render('final_state_machine', format='png', cleanup=True)
+    print("Граф сохранен как 'final_state_machine.png'")
+dka = nka_to_dka(grammar)
+print(dka)
+make_final_graph(dka)
