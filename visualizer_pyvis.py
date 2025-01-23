9e38a7aa1a66b279609f9039251f1d0eb65862df
Fivii12 <dryg882@gmail.com>
2025-01-23 12:46:18 +0300

    2

--- visualizer_pyvis.py
+++ visualizer_pyvis.py
@@ -1,5 +1,5 @@
+from graphviz import Digraph
 import networkx as nx
-from pyvis.network import Network
 
 
 def read_file(file_path):
@@ -8,24 +8,27 @@ def read_file(file_path):
 
     # Удаляем пустые строки и разделяем строки по табуляции
     lines = [line.strip().replace(' ', '').replace('\t', '') for line in lines if line.strip()]
-    header = lines[1][1:].split(';')
-    transitions = lines[2:]
+    ygreks = lines[0][1:].split(';')  # Игреки (y)
+    header = lines[1][1:].split(';')  # Состояния
+    transitions = lines[2:]  # Переходы
 
-    return header, transitions
+    return ygreks, header, transitions
 
 
-def create_graph(header, transitions):
+def create_graph(ygreks, header, transitions):
     G = nx.DiGraph()
 
-    # Добавляем узлы
-    for state in header:
-        G.add_node(state)
+    # Добавляем узлы с y
+    for index, state in enumerate(header):
+        y_label = ygreks[index]  # Получаем y для текущего состояния
+        node_label = f'{state}/{y_label}'  # Формируем метку узла: состояние/y
+        G.add_node(state, label=node_label)  # Добавляем узел с меткой
 
     # Добавляем ребра
     for row in transitions:
         line = row.split(';')
-        symbol = line[0]
-        parts = line[1:]
+        symbol = line[0]  # Символ перехода
+        parts = line[1:]  # Состояния перехода
 
         for index, state in enumerate(header):
             transition = parts[index].split(',')
@@ -44,24 +47,27 @@ def create_graph(header, transitions):
 
 
 def visualize_graph(G):
-    net = Network(notebook=True, directed=True)
+    dot = Digraph(comment='graph', format='png')
 
-    # Добавляем узлы и ребра в pyvis
-    for node in G.nodes():
-        net.add_node(node)
+    # Добавляем узлы с y
+    for node, data in G.nodes(data=True):
+        node_label = data.get('label', node)
+        dot.node(node, label=node_label)
+
 
     for edge in G.edges(data=True):
         src = edge[0]
         dst = edge[1]
         label = edge[2].get('label', '')
-        net.add_edge(src, dst, arrows='to', label=label)
+        dot.edge(src, dst, label=label)
 
-    net.show('graph.html')
+
+    dot.render('graph', cleanup=True)
 
 
 def main(file_path):
-    header, transitions = read_file(file_path)
-    Graph = create_graph(header, transitions)
+    ygreks, header, transitions = read_file(file_path)
+    Graph = create_graph(ygreks, header, transitions)
     visualize_graph(Graph)
 
 
